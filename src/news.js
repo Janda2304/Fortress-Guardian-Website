d7ae57f005ae1c9c5f656d588d2379d6b97734d4
Janda2304 <ondra.janous@email.cz>
2024-05-09 21:13:50 +0200

    news improvements
    Took 3 hours 27 minutes

--- src/news.js
+++ src/news.js
@@ -1,13 +1,51 @@
 document.addEventListener('DOMContentLoaded', function()
 {
-    let mainNews = document.querySelectorAll('.main-news-item .news-label-container');
+    let mainNewsContainers = document.querySelectorAll('.main-news-item');
+    let mainNewsLabels = document.querySelectorAll('.main-news-item .news-label-container');
+    let mainNewsImages = document.querySelectorAll('.main-news-item img');
     let news = document.querySelectorAll('.news-item');
+    let newsContainers = document.querySelectorAll('.news-item .news-label-container');
 
 
-    let i = 0;
+    let i = 0
 
-    mainNews.forEach((item) =>
+    console.log(mainNewsImages.length);
+
+    mainNewsLabels.forEach((item) =>
     {
+        let title = item.querySelector('h2');
+        let date = item.querySelector('p');
+
+        fetch('../data/news.json')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data =>
+            {
+                if (data[i].title === undefined)
+                {
+                    title.textContent = "NO-NEWS";
+                }
+                if (data[i].date === undefined)
+                {
+                    date.textContent = "January 1, 1970";
+                }
+                else
+                {
+                    title.textContent = data[i].title;
+                    date.textContent = data[i].date;
+                    mainNewsImages[i].src = data[i].image;
+                    mainNewsImages[i].alt = data[i].imageAlt;
+                    mainNewsContainers[i].href = data[i].devlogUrl;
+                }
+                i++;
+            });
+    });
+
+    news.forEach((item) => {
         let title = item.querySelector('h2');
         let date = item.querySelector('p');
         let image = item.querySelector('img');
@@ -33,6 +71,8 @@ document.addEventListener('DOMContentLoaded', function()
                 {
                     title.textContent = data[i].title;
                     date.textContent = data[i].date;
+                    image.src = data[i].image;
+                    image.alt = data[i].imageAlt;
                 }
                 i++;
             });
