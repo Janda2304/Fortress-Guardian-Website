311f1d4dda8713d874204ccb74cb1dffb7da08f9
Janda2304 <ondra.janous@email.cz>
2024-05-12 01:05:54 +0200

    final version
    Took 4 hours 47 minutes

--- src/script.js
+++ src/script.js
@@ -9,16 +9,20 @@ function toggleMenu()
     if (!isOpen)
     {
         menu.style.display = "flex";
-        menu.style.animation = "slide-in 0.75s ease-in-out";
+        menu.style.animation = "slide-in 0.4s ease-in-out";
         isOpen = true;
+        hamburger.classList.remove('fa-bars');
+        hamburger.classList.add('fa-times');
     }
     else
     {
         let computedStyle = window.getComputedStyle(menu);
         let currentPosition = computedStyle.getPropertyValue("transform");
 
-        menu.style.animation = "slide-out 0.75s ease-in-out";
+        menu.style.animation = "slide-out 0.4s ease-in-out";
         menu.style.transform = currentPosition;
+        hamburger.classList.remove('fa-times');
+        hamburger.classList.add('fa-bars');
         setTimeout(() =>
         {
             menu.style.display = "none";
@@ -36,5 +40,10 @@ document.addEventListener('DOMContentLoaded', function() {
             top: 0,
             behavior: 'smooth'
         });
+    });
+
+    document.getElementById('navigation').getElementsByClassName('play-button-container')[0].getElementsByClassName('button')[0].addEventListener('click', function()
+    {
+        window.location.href = "/src/about-game.html";
     });
 });
